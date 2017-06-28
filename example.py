abdb4301d7fe300ea41177ffaa1515a484fede52
saeeds255 <upse@yahoo.com>
2017-06-28 09:30:27 +0430

    Update example.py

--- example.py
+++ example.py
@@ -6,29 +6,29 @@ import sys
 BFX = Bitfinex('API_KEY', 'API_SECRET', 'https://api.bitfinex.com/v1')
 
 percent_profit = 5
-base_amount = 0.1
+base_amount = 0.3
 
 price = BFX.get_ticker('ETCUSD')
 buysig = float(price['last_price'])
-zoneh = buysig * (1 + percent_profit/100)
-sellsig = buysig - (zoneh - buysig)/3
+zoneh = float(buysig) * (1 + float(percent_profit)/100)
+sellsig = float(buysig - (zoneh - buysig)/3)
 zonel = sellsig - (zoneh - buysig)
 q1 = base_amount
-q2 = base_amount * 1.4
+q2 = q1 + base_amount * 1.4
 q2exit = q2 - q1
-q3 = base_amount * 1.4 * 1.4
+q3 = q2 + base_amount * 1.4 * 1.4
 q3exit = q3 - q2
-q4 = base_amount * 1.4 * 1.4 * 1.4
+q4 = q3 + base_amount * 1.4 * 1.4 * 1.4
 q4exit = q4 - q3
-q5 = base_amount * 1.4 * 1.4 * 1.4 * 1.4
+q5 = q4 + base_amount * 1.4 * 1.4 * 1.4 * 1.4
 q5exit = q5 - q4
-q6 = base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
+q6 = q5 + base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
 q6exit = q6 - q5
-q7 = base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
+q7 = q6 + base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
 q7exit = q7 - q6
-q8 = base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
+q8 = q7 + base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
 q8exit = q8 - q7
-q9 = base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
+q9 = q8 + base_amount * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4 * 1.4
 q9exit = q9 - q8
 
 myorder = BFX.buy('ETCUSD',q1,buysig,"limit")
@@ -36,7 +36,7 @@ print("Order ID: " + str(myorder['order_id']) + " / Price: " + str(myorder['pric
 time.sleep(1)
 market_price = float(price['last_price'])
 
-while (market_price < zoneh || market_price > sellsig):
+while (market_price < zoneh or market_price > sellsig):
   time.sleep(1)
   market_price = float(price['last_price'])
 
@@ -52,7 +52,7 @@ elif (market_price < sellsig):
   print("Order ID: " + str(myorder['order_id']) + " / Price: " + str(myorder['price']) + " / Canceled: " + str(myorder['is_cancelled']) + " / Live: " + str(myorder['is_live']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
   time.sleep(1)
   
-while (market_price > zonel || market_price < buysig):
+while (market_price > zonel or market_price < buysig):
   time.sleep(1)
   market_price = float(price['last_price'])
   
@@ -70,7 +70,7 @@ elif (market_price > buysig):
   
   #---------------------------------------------------------------------
   
-while (market_price < zoneh || market_price > sellsig):
+while (market_price < zoneh or market_price > sellsig):
   time.sleep(1)
   market_price = float(price['last_price'])
 
@@ -86,7 +86,7 @@ elif (market_price < sellsig):
   print("Order ID: " + str(myorder['order_id']) + " / Price: " + str(myorder['price']) + " / Canceled: " + str(myorder['is_cancelled']) + " / Live: " + str(myorder['is_live']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
   time.sleep(1)
   
-while (market_price > zonel || market_price < buysig):
+while (market_price > zonel or market_price < buysig):
   time.sleep(1)
   market_price = float(price['last_price'])
   
@@ -104,7 +104,7 @@ elif (market_price > buysig):
  
   #---------------------------------------------------------------------
   
-while (market_price < zoneh || market_price > sellsig):
+while (market_price < zoneh or market_price > sellsig):
   time.sleep(1)
   market_price = float(price['last_price'])
 
@@ -120,7 +120,7 @@ elif (market_price < sellsig):
   print("Order ID: " + str(myorder['order_id']) + " / Price: " + str(myorder['price']) + " / Canceled: " + str(myorder['is_cancelled']) + " / Live: " + str(myorder['is_live']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
   time.sleep(1)
   
-while (market_price > zonel || market_price < buysig):
+while (market_price > zonel or market_price < buysig):
   time.sleep(1)
   market_price = float(price['last_price'])
   
@@ -138,7 +138,7 @@ elif (market_price > buysig):
   
   #---------------------------------------------------------------------
   
-while (market_price < zoneh || market_price > sellsig):
+while (market_price < zoneh or market_price > sellsig):
   time.sleep(1)
   market_price = float(price['last_price'])
 
@@ -154,7 +154,7 @@ elif (market_price < sellsig):
   print("Order ID: " + str(myorder['order_id']) + " / Price: " + str(myorder['price']) + " / Canceled: " + str(myorder['is_cancelled']) + " / Live: " + str(myorder['is_live']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
   time.sleep(1)
   
-while (market_price > zonel || market_price < buysig):
+while (market_price > zonel or market_price < buysig):
   time.sleep(1)
   market_price = float(price['last_price'])
   
@@ -172,7 +172,7 @@ elif (market_price > buysig):
   
   #---------------------------------------------------------------------
   
-while (market_price < zoneh || market_price > sellsig):
+while (market_price < zoneh or market_price > sellsig):
   time.sleep(1)
   market_price = float(price['last_price'])
 
