e2372784c5293eed892b393342bc89c2da9a214a
saeeds255 <upse@yahoo.com>
2017-07-11 00:53:33 +0430

    Update BuyOrderSL.py

--- BuyOrderSL.py
+++ BuyOrderSL.py
@@ -7,10 +7,10 @@ BFX = Bitfinex('l48MCkTbzDkTAvDWC2u0OJ8BFwimI66rXn7S2OSE3Wy', 'GMudB4xN3zpcHR4tg
 
 def startit():
         slprice = 2365.5
-        myorder = BFX.postions()
+        myorder = BFX.positions()
         possymbol = str(myorder[0]['symbol'])
         posamount = float(myorder[0]['amount'])
-        print("Position symbol: " + possymbol + " /Position amount: " + posamount + " \n")
+        print("Position symbol: " + possymbol + " /Position amount: " + str(posamount) + " \n")
 
         price = BFX.get_ticker(possymbol)
         marketprice = float(price['last_price'])
@@ -20,7 +20,7 @@ def startit():
                 marketprice = float(price['last_price'])
 
         if (marketprice <= slprice):
-                myorder = BFX.sell(possymbol,posamount,market_price,"market")
+                myorder = BFX.sell(possymbol,posamount,marketprice,"market")
                 print("Order ID: " + str(myorder['order_id']) + " / Direction: " + str(myorder['side']) + " / Price: " + str(myorder['price']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
                 sys.exit()
 
