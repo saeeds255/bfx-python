3cfa61ab465e90328e9dfa2dfadd0d00a2680ec0
saeeds255 <upse@yahoo.com>
2017-07-24 11:16:58 +0430

    Update Bitfinex.py

--- Bitfinex.py
+++ Bitfinex.py
@@ -46,8 +46,8 @@ class Bitfinex:
 		url_path = '/order/new'
 		return self.construct_and_send(post_data, url_path)
 	
-	def buystop(self, symbol, amount, price, type_, exchange='bitfinex', ocoorder=True, buy_price_oco):
-		post_data = {'symbol' : symbol, 'amount' : str(amount), 'price' : str(price), 'exchange' : exchange, 'side' : 'buy', 'type' : type_, 'ocoorder' : ocoorder, 'buy_price_oco' : str(buy_price_oco)}
+	def buystop(self, symbol, amount, price, type_, exchange, ocoorder, buy_price_oco):
+		post_data = {'symbol' : symbol, 'amount' : str(amount), 'price' : str(price), 'exchange' : 'bitfinex', 'side' : 'buy', 'type' : type_, 'ocoorder' : ocoorder, 'buy_price_oco' : str(buy_price_oco)}
 		url_path = '/order/new'
 		return self.construct_and_send(post_data, url_path)
 
