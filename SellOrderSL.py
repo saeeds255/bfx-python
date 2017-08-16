2fb338226c60195c579fd9d533b3c72be45753d0
saeeds255 <upse@yahoo.com>
2017-08-17 00:13:01 +0430

    Update and rename SellOrderSL to SellOrderSL.py

---
+++ SellOrderSL.py
@@ -0,0 +1,28 @@
+
+from Bitfinex import Bitfinex
+import json
+import time
+import sys
+
+BFX = Bitfinex('API_KEY', 'API_SECRET', 'https://api.bitfinex.com/v1')
+
+def startit():
+        slprice = 2365.5
+        myorder = BFX.positions()
+        possymbol = str(myorder[0]['symbol'])
+        posamount = float(myorder[0]['amount'])
+        print("Position symbol: " + possymbol + " /Position amount: " + str(posamount) + " \n")
+
+        price = BFX.get_ticker(possymbol)
+        marketprice = float(price['last_price'])
+        while (marketprice < slprice):
+                time.sleep(2)
+                price = BFX.get_ticker(possymbol)
+                marketprice = float(price['last_price'])
+
+        if (marketprice >= slprice):
+                myorder = BFX.buy(possymbol,posamount,marketprice,"market")
+                print("Order ID: " + str(myorder['order_id']) + " / Direction: " + str(myorder['side']) + " / Price: " + str(myorder['price']) + " / Order amount: " + str(myorder['original_amount']) + " \n")
+                sys.exit()
+
+startit()
